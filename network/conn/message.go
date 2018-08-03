8d69cdced84d3916a1ade2a68c4f58d300e66f9d
jumper <jumper_2009@163.com>
2018-08-03 18:29:43 +0800

    笔记

--- network/conn/message.go
+++ network/conn/message.go
@@ -5,3 +5,8 @@ type Message struct {
 	MessageId      uint16
 	MessageContent interface{}
 }
+
+//1. session 使用同一个 chan []byte 将数据上传到解码层， 解码层需要获得是哪个session，然后使用对应的 parser 进行解码
+//可以考虑在 上传的数据前面加几个字节记录 sessionId ，这样 parser层就知道是哪个 session ， 而已获得对应的 parser实例进行解析
+//2. parser 实例考虑参考下 leaf
+//对于parser的定义 还关系到 message 的定义。 考虑直接采用 leaf 中的方式, 高度抽象 message,
